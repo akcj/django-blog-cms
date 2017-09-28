9bf5265cc1a0ee92eeadeafa7f6864a02e737a0a
akcj <akcj_zl@163.com>
2017-09-28 18:28:22 +0800

    update .gitignore

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,17 @@
 # coding: utf-8
 import xadmin
-from .models import User
+from .models import *
 
 from xadmin.views import BaseAdminView, CommAdminView
 
 
 class GlobalSetting(object):
     # 给xadmin后台加上一个站名
-    site_title = '<site title>'
+    site_title  = 'aaaaa'
     # 给xadmin后台footer部分加一个名称
-    site_footer = '<site footer>'
+    site_footer = 'bbbb'
     # 让xadmin后台的各种菜单可折叠
-    menu_style = 'accordion'
+    menu_style  = 'accordion'
 
 
 class BaseSetting(object):
@@ -20,17 +20,17 @@ class BaseSetting(object):
     # 启用更多主题选项
     use_bootswatch = True
 
-class UserAdmin(object):
+# class UserAdmin(object):
 
-    # 在后台中显示的字段
-    list_display = ('field1', 'field2', ......)
+#     # 在后台中显示的字段
+#     list_display = ('', '', )
 
-    # 给字段加上搜索功能
-    search_fields = ('field1', 'field2', ......)
+#     # 给字段加上搜索功能
+#     search_fields = ('', '', )
 
-    # 给字段加上过滤显示
-    list_filter = ('field1', 'field2', ......)
+    #     # 给字段加上过滤显示
+#     list_filter = ('', '', )
 
 
-xadmin.site.register(User, UserAdmin)
+# xadmin.site.register(User, UserAdmin)
 
